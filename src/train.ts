7e3bfae0f33de704725e9f6cc7420c560af713fa
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-07-23 01:01:49 -0700

    Removed train.ts

--- src/train.ts
+++
@@ -1,9 +0,0 @@
-console.log("Hello!");
-
-//TASK Q
-function hasProperty(a: object, b: string) {
-  const result = Object.keys(a).some((keys) => keys === b);
-  console.log(result);
-}
-hasProperty({ name: "BMW", model: "M3" }, "model");
-hasProperty({ name: "BMW", model: "M3" }, "name");
